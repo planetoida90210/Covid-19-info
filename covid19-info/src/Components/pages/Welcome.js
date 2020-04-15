3919f769e40dc854aa275ed904bb5946fe4f30e2
galeria koloru <galeriakoloru90210@gmail.com>
2020-04-15 23:51:43 +0200

    import assets to welcome page

--- covid19-info/src/Components/pages/Welcome.js
+++ covid19-info/src/Components/pages/Welcome.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import doctor from "../../assets/images/Welcome/doctor-mask.svg";
+import doctorMain from "../../assets/images/Welcome/doctor-gloves.svg";
+import virus from "../../assets/images/Welcome/bacteria-cell-single.svg";
 const Welcome = () => {
-  return <div>welcome page</div>;
+  return (
+    <div className='welcome-container'>
+      <h1 className='welcome-logo'>
+        c<span>logo</span>vid-19
+      </h1>
+      <h3 className='welcome-name'>coronavirus app</h3>
+      <img src={doctor} alt='doctor-logo' />
+      <img src={doctorMain} alt='doctor-logo' />
+      <img src={doctor} alt='doctor-logo' />
+      <p className='welcome-description'>
+        Sprawdź jak uchronić się przed Covid-19
+      </p>
+      <p className='welcome-description__secondary'>
+        Bądź świadomy zagrożenia i przeczytaj infomację dotyczące wirusa
+        SARS-CoV-2
+      </p>
+      <Link to='/'>
+        <button>więcej</button>
+      </Link>
+      <img src={virus} alt='bacteria-cell' />
+      <img src={virus} alt='bacteria-cell' />
+      <img src={virus} alt='bacteria-cell' />
+    </div>
+  );
 };
 
 export default Welcome;
