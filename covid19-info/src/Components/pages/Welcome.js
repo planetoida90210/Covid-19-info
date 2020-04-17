ef3b3332870eed0b8be18839a7ce85a5d2d55f08
galeria koloru <galeriakoloru90210@gmail.com>
2020-04-17 14:17:28 +0200

    add simple ui to welcome page on first mobile resolution

--- covid19-info/src/Components/pages/Welcome.js
+++ covid19-info/src/Components/pages/Welcome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/Welcome/main-logo.svg";
 import doctor from "../../assets/images/Welcome/doctor-mask.svg";
 import doctorMain from "../../assets/images/Welcome/doctor-gloves.svg";
 import virus from "../../assets/images/Welcome/bacteria-cell-single.svg";
@@ -7,12 +8,23 @@ const Welcome = () => {
   return (
     <div className='welcome-container'>
       <h1 className='welcome-logo'>
-        c<span>logo</span>vid-19
+        c
+        <span className='welcome-logo__svg'>
+          <img src={logo} alt='logo-virus' />
+        </span>
+        vid-19
       </h1>
       <h3 className='welcome-name'>coronavirus app</h3>
-      <img src={doctor} alt='doctor-logo' />
-      <img src={doctorMain} alt='doctor-logo' />
-      <img src={doctor} alt='doctor-logo' />
+      <div className='welcome-images-container'>
+        <div className='one'>
+          <img src={doctor} alt='doctor-logo' className='doctor-back' />
+        </div>
+        <img src={doctorMain} alt='doctor-logo' className='doctor-front' />
+        <div className='two'>
+          <img src={doctor} alt='doctor-logo' className='doctor-back' />
+        </div>
+      </div>
+
       <p className='welcome-description'>
         Sprawdź jak uchronić się przed Covid-19
       </p>
@@ -20,8 +32,8 @@ const Welcome = () => {
         Bądź świadomy zagrożenia i przeczytaj infomację dotyczące wirusa
         SARS-CoV-2
       </p>
-      <Link to='/'>
-        <button>więcej</button>
+      <Link className='welcome-login-link' to='/'>
+        <button className='welcome-login'>więcej</button>
       </Link>
       <img src={virus} alt='bacteria-cell' />
       <img src={virus} alt='bacteria-cell' />
