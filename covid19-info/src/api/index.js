3ad3522e39c7857bf328a01fb5cef2149b102d2e
planetoida90210 <galeriakoloru90210@gmail.com>
2020-07-28 18:08:29 +0200

    update api url

--- covid19-info/src/api/index.js
+++ covid19-info/src/api/index.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 // get global data
-const url = "https://cors-anywhere.herokuapp.com/http://corona.lmao.ninja/v2/";
+const url =
+  "https://cors-anywhere.herokuapp.com/https://disease.sh/v3/covid-19/";
 
 // get global data
 export const fetchGlobalData = async () => {
   try {
-    const { data } = await axios.get(`${url}/all?yesterday`);
+    const { data } = await axios.get(`${url}/all`);
     return data;
   } catch (error) {
     console.log("Error has occured, can't load data");
