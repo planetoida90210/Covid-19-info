52edab5c0d45356d13681d1648b0d24ecc3dbcfc
planetoida90210 <galeriakoloru90210@gmail.com>
2020-08-03 23:44:54 +0200

    add api data to mapbox

--- covid19-info/src/Components/cases/Map.js
+++ covid19-info/src/Components/cases/Map.js
@@ -8,15 +8,69 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoicGxhbmV0b2lkYTkwMjEwIiwiYSI6ImNrZGQyeGU4ZzE3d2kzMXQ5aXQydGRycDcifQ.iDPOx8aTyZDvXtcEGQjX2Q";
 const Map = () => {
   const mapboxElRef = useRef(null);
+  const fetcher = (url) =>
+    fetch(url)
+      .then((r) => r.json())
+      .then((data) =>
+        data.map((point, index) => ({
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [
+              point.coordinates.longitude,
+              point.coordinates.latitude,
+            ],
+          },
+          properties: {
+            id: index, // unique identifier in this case the index
+            country: point.country,
+            cases: point.stats.confirmed,
+            deaths: point.stats.deaths,
+          },
+        }))
+      );
+
+  // Fetching our data with swr package
+  const { data } = useSWR("https://corona.lmao.ninja/v2/jhucsse", fetcher);
+
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapboxElRef.current,
-      style: "mapbox://styles/planetoida90210/ckdeleaer56nu1io8blg36t0l",
-      center: [20, 51],
-      zoom: 0.1,
-    });
-    map.addControl(new mapboxgl.NavigationControl());
-  }, []);
+    if (data) {
+      const map = new mapboxgl.Map({
+        container: mapboxElRef.current,
+        style: "mapbox://styles/planetoida90210/ckdeleaer56nu1io8blg36t0l",
+        center: [20, 51],
+        zoom: 0.1,
+      });
+
+      // Call this method when the map is loaded
+      map.once("load", function () {
+        // Add our SOURCE
+        // with id "points"
+        map.addSource("points", {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: data,
+          },
+        });
+
+        // Add our layer
+        map.addLayer({
+          id: "circles",
+          source: "points", // this should be the id of the source
+          type: "circle",
+          // paint properties
+          paint: {
+            "circle-opacity": 0.75,
+            "circle-stroke-width": 1,
+            "circle-radius": 4,
+            "circle-color": "#FFEB3B",
+          },
+        });
+      });
+    }
+  }, [data]);
+
   return (
     <div className='active-map-container'>
       <div className='mapBox' ref={mapboxElRef} />
@@ -25,3 +79,13 @@ const Map = () => {
 };
 
 export default Map;
+
+// useEffect(() => {
+//     const map = new mapboxgl.Map({
+//       container: mapboxElRef.current,
+//       style: "mapbox://styles/planetoida90210/ckdeleaer56nu1io8blg36t0l",
+//       center: [20, 51],
+//       zoom: 0.1,
+//     });
+//     map.addControl(new mapboxgl.NavigationControl());
+//   }, []);
