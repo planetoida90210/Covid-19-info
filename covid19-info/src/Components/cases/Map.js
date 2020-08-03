44d398fb8f65f15b230a00f238dfb973391ca6eb
planetoida90210 <galeriakoloru90210@gmail.com>
2020-08-03 21:51:54 +0200

    add custom map

--- covid19-info/src/Components/cases/Map.js
+++ covid19-info/src/Components/cases/Map.js
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
 mapboxgl.accessToken =
   "pk.eyJ1IjoicGxhbmV0b2lkYTkwMjEwIiwiYSI6ImNrZGQyeGU4ZzE3d2kzMXQ5aXQydGRycDcifQ.iDPOx8aTyZDvXtcEGQjX2Q";
 const Map = () => {
@@ -7,9 +9,9 @@ const Map = () => {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapboxElRef.current,
-      style: "mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k",
-      center: [0, 0],
-      zoom: -1,
+      style: "mapbox://styles/planetoida90210/ckdeleaer56nu1io8blg36t0l",
+      center: [20, 51],
+      zoom: 0.1,
     });
     map.addControl(new mapboxgl.NavigationControl());
   }, []);
