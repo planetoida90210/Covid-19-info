ec5cd8e885a096f747fb9d408e3ad557e2ae91af
planetoida90210 <galeriakoloru90210@gmail.com>
2020-08-13 19:54:10 +0200

    change symptom details to upper component to stop prop driling

--- covid19-info/src/Components/symptoms/SingleSymptom.js
+++ covid19-info/src/Components/symptoms/SingleSymptom.js
@@ -4,46 +4,13 @@ import SingleCard from "./SingleCard";
 import Arrow from "./Arrow";
 import Dots from "./Dots";
 
-import drycough from "../../assets/images/Home/drycough.svg";
-import fever from "../../assets/images/Home/fever.svg";
-import dizzy from "../../assets/images/Home/dizzy.svg";
-
-const SingleSymptom = () => {
-  const symptomsData = [
-    {
-      id: 1,
-      title: "suchy kaszel",
-      shortDesc: "więcej niż połowa zarażonych posiada takie objawy",
-      longDescHeader: "suchy kaszel w przebiegu COVID-19",
-      longDesc:
-        "Wśród dominujących symptomów wskazujących na COVID-19 znajdują się gorączka i zmęczenie, a także samopoczucie podobne do tego pojawiającego się w przebiegu przeziębienia i grypy. U mniej więcej połowy osób zakażonych koronawirusem SARS-CoV-2 stwierdza się również kaszel. W przebiegu COVID-19 kaszel ma charakter suchy i uporczywy, co wynika z podrażnienia tkanki płucnej. Podczas kaszlu pojawiają się też bóle mięśniowe i spłycenie oddechu (duszność). Wraz z postępowaniem choroby w płucach gromadzi się płyn, dlatego z czasem trudności z oddychaniem mogą się nasilać.",
-      logo: `${drycough}`,
-    },
-    {
-      id: 2,
-      title: "gorączka",
-      shortDesc: "wysoka gorączka zwykle przekraczająca 38◦C",
-      longDescHeader: "wysoka gorączka w przebiegu COVID-19",
-      longDesc:
-        "Wzrost temperatury ciała to jeden z podstawowych wskaźników informujących o infekcji. Pojawienie się wysokiej gorączki to objaw, który może wskazywać na zakażenie koronawirusem COVID-19, o ile występuje równocześnie z innymi symptomami takimi jak suchy kaszel i duszności. Powszechnie uważa się, że temperatura 36,6 st. C to umowna wartość prawidłowa dla zdrowego człowieka. Obecnie uważa się, że w granicach normy znajdują się wyniki od 36,4 do 37 st. C.",
-      logo: `${fever}`,
-    },
-    {
-      id: 3,
-      title: "zmęczenie",
-      shortDesc: "ból mięśni, poczucie osłabienia organizmu",
-      longDescHeader: "osłabienie w przebiegu COVID-19",
-      longDesc:
-        "W niektórych przypadkach infekcji COVID-19 mogą towarzyszyć: złe samopoczucie, dezorientacja i bardzo silne zmęczenie. Zalicza się je do najczęściej wymienianych nietypowych objawów zakażenia, które występują łącznie z kaszlem i gorączką. Zmęczenie jest bardzo rzadko zgłaszane przez chorych na COVID-19 jako jedyny występujący objaw. Z kolei eksperci amerykańskich Centrów Kontroli i Prewencji Chorób (CDC) podkreślają, że silna dezorientacja czy problemy z obudzeniem się, z utrzymaniem świadomości mogą być objawami ostrzegawczymi, świadczącymi o poważniejszym problemie zdrowotnym. Osoby, u których występują, zwłaszcza łącznie z innymi objawami, jak np. zasinienie ust, trudności z oddychaniem czy ból w klatce piersiowej, powinny natychmiast wezwać pomoc medyczną.",
-      logo: `${dizzy}`,
-    },
-  ];
+const SingleSymptom = ({ symptoms }) => {
   //checking actual width of window
   const widthWindow = () => window.innerWidth;
   // get reference to symptom data, that we manage later to create carousel effect
-  const firstCard = symptomsData[0];
-  const secondCard = symptomsData[1];
-  const lastCard = symptomsData[symptomsData.length - 1];
+  const firstCard = symptoms[0];
+  const secondCard = symptoms[1];
+  const lastCard = symptoms[symptoms.length - 1];
 
   // state hook
   const [state, setState] = useState({
@@ -77,12 +44,12 @@ const SingleSymptom = () => {
   const smoothTransition = () => {
     let cards = [];
     // We're at the last slide.
-    if (activeIndex === symptomsData.length - 1)
-      cards = [symptomsData[symptomsData.length - 2], lastCard, firstCard];
+    if (activeIndex === symptoms.length - 1)
+      cards = [symptoms[symptoms.length - 2], lastCard, firstCard];
     // We're back at the first slide. Just reset to how it was on initial render
     else if (activeIndex === 0) cards = [lastCard, firstCard, secondCard];
     // Create an array of the previous last slide, and the next two slides that follow it.
-    else cards = symptomsData.slice(activeIndex - 1, activeIndex + 2);
+    else cards = symptoms.slice(activeIndex - 1, activeIndex + 2);
 
     setState({
       ...state,
@@ -96,16 +63,14 @@ const SingleSymptom = () => {
     setState({
       ...state,
       translate: translate + widthWindow(),
-      activeIndex:
-        activeIndex === symptomsData.length - 1 ? 0 : activeIndex + 1,
+      activeIndex: activeIndex === symptoms.length - 1 ? 0 : activeIndex + 1,
     });
   // handling prev slide button
   const prevSlide = () =>
     setState({
       ...state,
       translate: 0,
-      activeIndex:
-        activeIndex === 0 ? symptomsData.length - 1 : activeIndex - 1,
+      activeIndex: activeIndex === 0 ? symptoms.length - 1 : activeIndex - 1,
     });
 
   return (
@@ -123,7 +88,7 @@ const SingleSymptom = () => {
         ))}
       </div>
       <Arrow direction='right' handleClick={nextSlide} />
-      <Dots activeIndex={activeIndex} cards={symptomsData} />
+      <Dots activeIndex={activeIndex} cards={symptoms} />
     </div>
   );
 };
--- covid19-info/src/Components/symptoms/Symptoms.js
+++ covid19-info/src/Components/symptoms/Symptoms.js
@@ -1,10 +1,45 @@
 import React from "react";
 import SingleSymptom from "./SingleSymptom";
+import SymptomDetails from "./SymptomDetails";
+
+import drycough from "../../assets/images/Home/drycough.svg";
+import fever from "../../assets/images/Home/fever.svg";
+import dizzy from "../../assets/images/Home/dizzy.svg";
 
 const Symptoms = () => {
+  const symptomsData = [
+    {
+      id: 1,
+      title: "suchy kaszel",
+      shortDesc: "więcej niż połowa zarażonych posiada takie objawy",
+      longDescHeader: "suchy kaszel w przebiegu COVID-19",
+      longDesc:
+        "Wśród dominujących symptomów wskazujących na COVID-19 znajdują się gorączka i zmęczenie, a także samopoczucie podobne do tego pojawiającego się w przebiegu przeziębienia i grypy. U mniej więcej połowy osób zakażonych koronawirusem SARS-CoV-2 stwierdza się również kaszel. W przebiegu COVID-19 kaszel ma charakter suchy i uporczywy, co wynika z podrażnienia tkanki płucnej. Podczas kaszlu pojawiają się też bóle mięśniowe i spłycenie oddechu (duszność). Wraz z postępowaniem choroby w płucach gromadzi się płyn, dlatego z czasem trudności z oddychaniem mogą się nasilać.",
+      logo: `${drycough}`,
+    },
+    {
+      id: 2,
+      title: "gorączka",
+      shortDesc: "wysoka gorączka zwykle przekraczająca 38◦C",
+      longDescHeader: "wysoka gorączka w przebiegu COVID-19",
+      longDesc:
+        "Wzrost temperatury ciała to jeden z podstawowych wskaźników informujących o infekcji. Pojawienie się wysokiej gorączki to objaw, który może wskazywać na zakażenie koronawirusem COVID-19, o ile występuje równocześnie z innymi symptomami takimi jak suchy kaszel i duszności. Powszechnie uważa się, że temperatura 36,6 st. C to umowna wartość prawidłowa dla zdrowego człowieka. Obecnie uważa się, że w granicach normy znajdują się wyniki od 36,4 do 37 st. C.",
+      logo: `${fever}`,
+    },
+    {
+      id: 3,
+      title: "zmęczenie",
+      shortDesc: "ból mięśni, poczucie osłabienia organizmu",
+      longDescHeader: "osłabienie w przebiegu COVID-19",
+      longDesc:
+        "W niektórych przypadkach infekcji COVID-19 mogą towarzyszyć: złe samopoczucie, dezorientacja i bardzo silne zmęczenie. Zalicza się je do najczęściej wymienianych nietypowych objawów zakażenia, które występują łącznie z kaszlem i gorączką. Zmęczenie jest bardzo rzadko zgłaszane przez chorych na COVID-19 jako jedyny występujący objaw. Z kolei eksperci amerykańskich Centrów Kontroli i Prewencji Chorób (CDC) podkreślają, że silna dezorientacja czy problemy z obudzeniem się, z utrzymaniem świadomości mogą być objawami ostrzegawczymi, świadczącymi o poważniejszym problemie zdrowotnym. Osoby, u których występują, zwłaszcza łącznie z innymi objawami, jak np. zasinienie ust, trudności z oddychaniem czy ból w klatce piersiowej, powinny natychmiast wezwać pomoc medyczną.",
+      logo: `${dizzy}`,
+    },
+  ];
   return (
     <div>
-      <SingleSymptom />
+      <SingleSymptom symptoms={symptomsData} />
+      <SymptomDetails />
     </div>
   );
 };
