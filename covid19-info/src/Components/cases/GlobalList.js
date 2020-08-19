b31e8f15fd5bd5a7b63b40dae2ed69385aa3a2b7
planetoida90210 <galeriakoloru90210@gmail.com>
2020-08-19 16:27:31 +0200

    add div to global list summary of all active,deaths,recovered cases

--- covid19-info/src/Components/cases/GlobalList.js
+++ covid19-info/src/Components/cases/GlobalList.js
@@ -1,23 +1,24 @@
 import React from "react";
 
 const GlobalList = ({ countriesData, globalData }) => {
-  const { active, deaths, recovered } = globalData;
+  const { active, recovered, deaths } = globalData;
   return (
     <div className='global-country-list-container'>
       <div className='global-country-list-summary'>
-        <h5 className='global-cases_description'>zakażonych</h5>
-        <p className='global-cases_counter'>
+        <h5 className='global-cases-detail_description'>zakażonych</h5>
+        <p className='global-cases-detail_counter'>
           {Intl.NumberFormat().format(active)}
         </p>
-        <h5 className='global-cases_description'>wyzdrowiało</h5>
-        <p className='global-cases_counter'>
+        <h5 className='global-cases-detail_description'>wyzdrowiało</h5>
+        <p className='global-cases-detail_counter'>
           {Intl.NumberFormat().format(recovered)}
         </p>
-        <h5 className='global-cases_description'>umarło</h5>
-        <p className='global-cases_counter'>
+        <h5 className='global-cases-detail_description'>umarło</h5>
+        <p className='global-cases-detail_counter'>
           {Intl.NumberFormat().format(deaths)}
         </p>
       </div>
+      <ul className='global-country-table'></ul>
     </div>
   );
 };
