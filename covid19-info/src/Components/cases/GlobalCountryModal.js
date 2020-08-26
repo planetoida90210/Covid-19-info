de9dc3f4f8ce4623141817f7bbcb66586889d48d
planetoida90210 <galeriakoloru90210@gmail.com>
2020-08-27 00:29:09 +0200

    add modal on click, on single country

---
+++ covid19-info/src/Components/cases/GlobalCountryModal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GlobalCountryModal = ({ show, closeModal }) => {
+  return <div>elo</div>;
+};
+
+export default GlobalCountryModal;
--- covid19-info/src/Components/cases/GlobalSingleCountry.js
+++ covid19-info/src/Components/cases/GlobalSingleCountry.js
@@ -1,24 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
+import GlobalCountryModal from "./GlobalCountryModal";
 const GlobalSingleCountry = ({
   country: { countryInfo, country, cases, recovered, deaths },
 }) => {
+  const [show, setShow] = useState(false);
+  const openModal = () => setShow(true);
+  const closeModal = () => setShow(false);
+
   return (
-    <div className='global-single-country-container'>
-      <img
-        src={countryInfo.flag}
-        alt='flag'
-        className='global-single-country-flag'
-      />
-      <p className='global-country_data'>{country}</p>
-      <p className='global-country_data'>{Intl.NumberFormat().format(cases)}</p>
-      <p className='global-country_data'>
-        {Intl.NumberFormat().format(recovered)}
-      </p>
-      <p className='global-country_data'>
-        {Intl.NumberFormat().format(deaths)}
-      </p>
-    </div>
+    <>
+      {!show ? (
+        <div onClick={openModal} className='global-single-country-container'>
+          <img
+            src={countryInfo.flag}
+            alt='flag'
+            className='global-single-country-flag'
+          />
+          <p className='global-country_data'>{country}</p>
+          <p className='global-country_data'>
+            {Intl.NumberFormat().format(cases)}
+          </p>
+          <p className='global-country_data'>
+            {Intl.NumberFormat().format(recovered)}
+          </p>
+          <p className='global-country_data'>
+            {Intl.NumberFormat().format(deaths)}
+          </p>
+        </div>
+      ) : (
+        <GlobalCountryModal closeModal={closeModal} show={show} />
+      )}
+    </>
   );
 };
 
