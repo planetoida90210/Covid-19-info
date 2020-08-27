5e6c3d7ce0526656d7af4e2ef4890ffc349baac8
planetoida90210 <galeriakoloru90210@gmail.com>
2020-08-28 00:44:59 +0200

    add daily data for modal

--- covid19-info/src/Components/cases/GlobalCountryModal.js
+++ covid19-info/src/Components/cases/GlobalCountryModal.js
@@ -11,8 +11,19 @@ const GlobalCountryModal = ({ show, closeModal, country }) => {
           className={show ? "show-modal" : "hide-modal"}
           onClick={closeModal}
         >
-          <img src={country.countryInfo.flag} alt='flag' />
-          <p>{country.country}</p>
+          {/* <img
+            src={country.countryInfo.flag}
+            alt='flag'
+            className='modal-country-flag'
+          /> */}
+          <div className='modal-cases-data-container'>
+            <p>{country.cases}</p>
+            <p>{country.recovered}</p>
+            <p>{country.deaths}</p>
+            <p>{country.todayCases}</p>
+            <p>{country.todayRecovered}</p>
+            <p>{country.todayDeaths}</p>
+          </div>
         </div>
       </div>
     </>
