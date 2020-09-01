cbbcd983a6470da60bd7d480dec57d14de727e66
planetoida90210 <galeriakoloru90210@gmail.com>
2020-09-01 20:42:55 +0200

    add autocomplete div to searchbar

--- covid19-info/src/Components/cases/InputButton.js
+++ covid19-info/src/Components/cases/InputButton.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const InputButton = ({ countries }) => {
   return (
     <div className='input-map-button'>
-      <input type='search' placeholder='Szukaj' />
+      <input
+        className='input-search-countries'
+        type='text'
+        placeholder='Szukaj'
+      />
+      <div className='autocomplete-search-bar'>
+        <ul className='autocomplete-list'>
+          <li>elo</li>
+        </ul>
+      </div>
     </div>
   );
 };
