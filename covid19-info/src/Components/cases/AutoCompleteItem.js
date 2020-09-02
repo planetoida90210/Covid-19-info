705bb4397d9211167e7a4559554317c75662b067
planetoida90210 <galeriakoloru90210@gmail.com>
2020-09-02 21:38:10 +0200

    add autocomplete single country to searchbar

---
+++ covid19-info/src/Components/cases/AutoCompleteItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AutoCompleteItem = () => {
+  return (
+    <>
+      <li className='autocomplete-search-country'>nazwa kraju</li>
+    </>
+  );
+};
+
+export default AutoCompleteItem;
--- covid19-info/src/Components/cases/InputButton.js
+++ covid19-info/src/Components/cases/InputButton.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from "react";
-
+import AutocompleteItem from "./AutoCompleteItem";
 const InputButton = ({ countries }) => {
+  const [isVisible, setVisibility] = useState(false);
+
+  const showAutoSuggestion = () => setVisibility(true);
+  const hideAutoSuggestion = () => setVisibility(false);
   return (
     <div className='input-map-button'>
       <input
         className='input-search-countries'
         type='text'
         placeholder='Szukaj'
+        onClick={showAutoSuggestion}
       />
-      <div className='autocomplete-search-bar'>
+      <div
+        className={
+          isVisible ? "autocomplete-search-bar" : "autocomplete-search-bar_hide"
+        }
+      >
         <ul className='autocomplete-list'>
-          <li>elo</li>
+          <AutocompleteItem />
+          <AutocompleteItem />
+          <AutocompleteItem />
         </ul>
       </div>
     </div>
