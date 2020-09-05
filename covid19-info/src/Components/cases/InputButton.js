329461ef9d484c1d889b966bb47d884c72b1e734
planetoida90210 <galeriakoloru90210@gmail.com>
2020-09-05 13:20:19 +0200

    add logic to show/hide suggestions in autocomplete component

--- covid19-info/src/Components/cases/InputButton.js
+++ covid19-info/src/Components/cases/InputButton.js
@@ -1,12 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import AutocompleteItem from "./AutoCompleteItem";
 const InputButton = ({ countries }) => {
   const [isVisible, setVisibility] = useState(false);
-
+  const searchContainer = useRef(null);
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  const handleClickOutside = (e) => {
+    if (
+      searchContainer.current &&
+      !searchContainer.current.contains(e.target)
+    ) {
+      hideAutoSuggestion();
+    }
+  };
   const showAutoSuggestion = () => setVisibility(true);
   const hideAutoSuggestion = () => setVisibility(false);
   return (
-    <div className='input-map-button'>
+    <div className='input-map-button' ref={searchContainer}>
       <input
         className='input-search-countries'
         type='text'
